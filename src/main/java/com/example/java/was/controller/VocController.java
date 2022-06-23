17dccea690ecc94b1ae5ba309aba0d04d8f07f85
guruguruzom <guruguruzom@gmail.com>
2022-06-23 16:25:43 +0900

    로그 설정

--- src/main/java/com/example/java/was/controller/VocController.java
+++ src/main/java/com/example/java/was/controller/VocController.java
@@ -2,6 +2,8 @@ package com.example.java.was.controller;
 
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,8 @@ import com.example.java.was.service.impl.VocServiceImpl;
 @RestController
 public class VocController {
 
+	private static Logger logger = LoggerFactory.getLogger(VocController.class);
+	
 	@Autowired VocServiceImpl vocServiceImpl;
 	
 	/**
@@ -21,6 +25,8 @@ public class VocController {
 	 */
 	@PostMapping("/api/registration")
     public void registration(@RequestBody VocDto vocDto) {
+		
+		logger.info("service class Hello : " + vocDto.toString());
 		
 		System.out.println(vocDto.toString());
 		System.out.println(vocDto.getReason());
