a54374c2f2fa745a61b9966ce906211b6705cb43
guruguruzom <guruguruzom@gmail.com>
2022-06-24 11:50:47 +0900

    배상 등록 api 제작

--- src/main/java/com/example/java/was/service/CompensationService.java
+++ src/main/java/com/example/java/was/service/CompensationService.java
@@ -1,11 +1,25 @@
 package com.example.java.was.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.java.was.dto.CompensationDto;
+import com.example.java.was.entity.CompensationVo;
+import com.example.java.was.repository.CompensationRepository;
 import com.example.java.was.service.impl.CompensationServiceImpl;
 
 public class CompensationService implements CompensationServiceImpl{
 	
+	@Autowired CompensationRepository compensationRepository; 
+	
 	public void setCompensation(CompensationDto compensationDto) {
+		CompensationVo compensationVo = CompensationVo.builder()
+										.vocId(compensationDto.getId())
+										.penaltyId(compensationDto.getPenaltyId())
+										.reparations(compensationDto.getReparations())
+										.comment(compensationDto.getComment())
+										.build();
 		
-	}
+		compensationRepository.save(compensationVo);
+		
+	}		
 }
